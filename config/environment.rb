aac437621cbf4711eb80edc737544f713e5e6d1d
aceimpact <kei2613@icloud.com>
2019-03-31 22:03:42 +0900

    8

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => Settings.gmail[:email],
+  :password       => Settings.gmail[:password],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}

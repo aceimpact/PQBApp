61608518c5fa2951007ba29a309f8ea6ffc8306d
aceimpact <kei2613@icloud.com>
2019-04-01 21:34:57 +0900

    Mail

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => Settings.gmail[:email],
-  :password       => Settings.gmail[:password],
+  :user_name      => Settings.sendgrid[:user_name],
+  :password       => Settings.sendgrid[:password],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }

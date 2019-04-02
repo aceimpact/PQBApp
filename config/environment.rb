577dc7a94e4435e57f6f385f9b6b4940eac5b8bf
aceimpact <kei2613@icloud.com>
2019-04-02 21:51:18 +0900

    メール機能

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,10 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
-  :port           => '587',
+  :port           => 587,
   :authentication => :plain,
   :user_name      => Settings.sendgrid[:user_name],
   :password       => Settings.sendgrid[:password],

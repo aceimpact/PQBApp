0131517c8986b422fd44bca2a9ffa630bb5ebbb6
aceimpact <kei2613@icloud.com>
2019-04-07 00:46:04 +0900

    deviseのアドレス微調整

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'ace4869kei@gmail.com'
+  config.mailer_sender = Settings.sendgrid[:user_name]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'

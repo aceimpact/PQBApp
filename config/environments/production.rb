fbd48a6dc5ed5d8046363e4406fac999fb59d819
aceimpact <kei2613@icloud.com>
2019-04-09 01:35:37 +0900

    productionの変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -89,16 +89,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'http://pqbapp.herokuapp.com' }
-    config.action_mailer.raise_delivery_errors = false
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => 587,
-      :authentication => :plain,
-      :user_name      => Settings.sendgrid[:user_name],
-      :password       => Settings.sendgrid[:password],
-      :domain         => 'pqbapp.herokuapp.com',
-      :enable_starttls_auto => true
-    }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net' }
+   config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => 587,
+    :authentication => :plain,
+    :user_name      => Settings.sendgrid[:user_name],
+    :password       => Settings.sendgrid[:password],
+    :domain         => 'pqbapp.herokuapp.com',
+    :enable_starttls_auto => true
+  }
 end

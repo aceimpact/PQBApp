ea609c0552f6b6888a616c7c02f5256be85aeb77
aceimpact <kei2613@icloud.com>
2019-04-09 23:38:10 +0900

    production変更した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net' }
-   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'pqbapp.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => 587,
